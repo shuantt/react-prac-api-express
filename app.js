4de47fe7928c073ae9b40d5428b3a3c0afc1e1a5
shuantt <tehsuan.tht@gmail.com>
2024-11-04 17:02:33 +0800

    chore: 加入 cors 套件
    issue: week4-hw3

--- app.js
+++ app.js
@@ -3,9 +3,10 @@ require('dotenv').config({ path: "./config.env" });
 const express = require('express');
 const bodyParser = require('body-parser');
 const authRoutes = require('./routes/auth');
+const cors = require('cors');
 
 const app = express();
-
+app.use(cors());
 app.use(bodyParser.json());
 
 // 使用路由
