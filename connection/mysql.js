b723de870f25f1bcf3a42ea44c46611425e09aca
shuantt <tehsuan.tht@gmail.com>
2024-11-20 15:36:35 +0800

    fix: 新增 DB port 號設定
    - 因 zeabur 使用對外 domain 和指定 port 號，因此連線時須新增 port 參數

--- connection/mysql.js
+++ connection/mysql.js
@@ -4,6 +4,7 @@ const mysql = require('mysql2');
 // 建立 MySQL 連接
 const db = mysql.createConnection({
     host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
