d3b10eb1ba46b1552fb714f12ca103da211c2764
shuantt <tehsuan.tht@gmail.com>
2024-11-20 15:10:54 +0800

    fix: 修改資料庫連線變數名稱
    - 嘗試部署到 zeabur，測試如何變數

--- connection/mysql.js
+++ connection/mysql.js
@@ -3,10 +3,10 @@ const mysql = require('mysql2');
 
 // 建立 MySQL 連接
 const db = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
+    host: DB_HOST,
+    user: DB_USER,
+    password: DB_PASSWORD,
+    database: DB_NAME,
 });
 
 module.exports = db;
